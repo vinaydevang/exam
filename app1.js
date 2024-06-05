2d3e3d6152970110d998061fbb0461cb79038133
Vinay M <104661631+vini-1155@users.noreply.github.com>
2024-06-05 14:35:35 +0530

    first-commit

---
+++ app1.js
@@ -0,0 +1,22 @@
+angular.module('communityCenterApp',[]).controller('ServiceController',['$scope',function($scope){
+    $scope.services=[];
+    $scope.newService={
+        name:'',
+        description:'',
+        date:''
+    };
+    $scope.addService=function(){
+        if($scope.newService.name && $scope.newService.description && $scope.newService.date)
+            {
+                $scope.services.push({
+                    name:$scope.newService.name,
+                    description:$scope.newService.description,
+                    date:$scope.newService.date
+                });
+                $scope.newService.name='';
+                $scope.newService.description='';
+                $scope.newService.date='';
+            }
+    }
+}
+])
